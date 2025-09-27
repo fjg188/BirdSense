21eb4b0ee4303b5b754fedc4971cd74570140e76
Felix Grimm <87872870+flflflf@users.noreply.github.com>
2025-09-27 18:12:54 -0400

    added analytics

--- birdsense/app/about/layout.tsx
+++ birdsense/app/about/layout.tsx
@@ -1,9 +1,13 @@
 import type React from "react"
+import { Analytics } from "@vercel/analytics/next"
+
 
 export default function HowItWorksLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <>{children}</>
+  return (<>{children}
+    <Analytics /></>
+  )
 }
